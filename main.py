a05251737df1f15c8dd2651fad4c393e4a8aec01
Matheus A. Araujo <75196977+matheusaraujotrd@users.noreply.github.com>
2022-12-23 13:06:10 -0300

    +Finetuning exporter and scrapper modules.

---
+++ main.py
@@ -0,0 +1,6 @@
+import scrapper
+import exporter
+import html_parser
+
+# Team enumerators
+team_enum={"ARI":"Arizona Coyotes","BOS":"Boston Bruins","CAR":"Carolina Hurricanes",}
--- scrapper.py
+++ scrapper.py
@@ -4,19 +4,24 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import html_parser
-import pandas as pd
+import exporter
 
-s = Service("/usr/lib/chromium-browser/chromedriver")
-option = webdriver.ChromeOptions()
-option.add_argument("headless")
-option.add_argument("--log-level=3")
-driver = webdriver.Chrome(options=option, service=s)
-driver.get("https://www.nhl.com/redwings/roster")
-urls = len(driver.find_elements(By.CSS_SELECTOR, "div.name-col__list"))
-for x in range(urls):
-    player=[]
-    elements = driver.find_elements(By.CSS_SELECTOR, "div.name-col__list")
-    driver.execute_script("arguments[0].click();", elements[x])
-    player.append(html_parser.parser(driver.page_source))
-    driver.back()
-    print(player)
+urls_teams_enum={}
+player_data_df=[]
+
+def running_scrapper(team="all"):
+    s = Service("/usr/lib/chromium-browser/chromedriver")
+    option = webdriver.ChromeOptions()
+    option.add_argument("headless")
+    option.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=option, service=s)
+    driver.get("https://www.nhl.com/redwings/roster")
+    urls = len(driver.find_elements(By.CSS_SELECTOR, "div.name-col__list"))
+    for x in range(urls):
+        player=[]
+        elements = driver.find_elements(By.CSS_SELECTOR, "div.name-col__list")
+        driver.execute_script("arguments[0].click();", elements[x])
+        player.append(html_parser.parser(driver.page_source))
+        driver.back()
+        player_data_df.append(exporter.write_player_data_to_df(player))
+    
