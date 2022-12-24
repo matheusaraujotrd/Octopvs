e4751866f7d3d7dfdccb6f733acf569ca366dcae
Matheus A. Araujo <75196977+matheusaraujotrd@users.noreply.github.com>
2022-12-24 20:58:15 -0300

    +Fixed exporter file management

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import scrapper
 import exporter
 import html_parser
 
+def welcome():
+    pass
+
 # Team enumerators
 team_enum={"ARI":"Arizona Coyotes","BOS":"Boston Bruins","CAR":"Carolina Hurricanes",}
 
