c2d216228e46b9ea3e75b6d3039b8d1fc3647af4
Matheus A. Araujo <75196977+matheusaraujotrd@users.noreply.github.com>
2022-12-27 20:57:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,4 @@ import scrapper
 import exporter
 import html_parser
 
-def welcome():
-    pass
-
-# Team enumerators
-team_enum={"ARI":"Arizona Coyotes","BOS":"Boston Bruins","CAR":"Carolina Hurricanes",}
-
 scrapper.scrappy_execute()
